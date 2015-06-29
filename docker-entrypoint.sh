199d9716bf476e85542ca2e6b796bc8b0d07f7da
dmfenton <dfenton@esri.com>
2015-06-29 12:05:47 -0400

    more hardcoding of pgcache

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
-
 set -e
-
 # PostgreSQL database name for koop
 KOOP_DATABASE=${KOOP_DATABASE:-koopdev}
+. $APP_DIR/config/scripts/create_db.sh
+check_koop_db
+cd $APP_DIR
+echo "Starting the koop server..."
+exec node server.js
 
-if [ "$1" = 'koop' ]; then
-  if [ "$2" = 'postgis' ]; then
-    # Create the database
-    echo "params passed correctly"
-    . $APP_DIR/config/scripts/create_db.sh
-    check_koop_db
-  fi
-
-  cd $APP_DIR
-  echo "Starting the koop server..."
-  exec node server.js
-fi
-
-exec "$@"
